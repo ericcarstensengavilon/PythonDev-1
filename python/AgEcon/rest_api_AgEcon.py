5b034948fff182ab0b0f81a88d65da4f643d33c6
Eric Carstensen <imjacksdad@gmail.com>
2021-02-04 09:59:04 -0600

    Initial Upload

---
+++ python/AgEcon/rest_api_AgEcon.py
@@ -0,0 +1,12 @@
+#import json
+import requests
+
+...
+#API_KEY  = '5E67F11C-DBE0-40CE-A72F-5F8F0EC566D7'
+#api_url_base = 'https://apps.fas.usda.gov/PSDOnlineDataServices/api/CommodityData/GetCommodityDataByYear?'
+
+
+res = requests.get('https://scotch.io')
+
+print(res)
+
